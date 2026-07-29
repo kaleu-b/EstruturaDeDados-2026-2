01ed9ab87c63289be7df5894b256580795647dd8
kaleu-b <kaleu.b@aluno.ifsc.edu.br>
2026-07-29 01:01:49 -0300

    Adicionando métodos para adicionar projeto, que pedem qtde. de funcionários e o gerente encarregado.

--- Atividade00/Ex01/src/Main.java
+++ Atividade00/Ex01/src/Main.java
@@ -3,21 +3,21 @@ public class Main{
     private Projeto[] projetos = new Projeto[100];
     private Subordinado[] funcionarios = new Subordinado[100];
 
-    funcionarios[0] = new Subordinado(1000.0);
-
     void main() {
+        while (true) {
             IO.println("-------------PROJETOS------------");
             IO.println("1 - Cadastrar Projetos");
             IO.println("2 - Mostrar Projetos");
 
             String escolha = IO.readln("Escolha: ");
 
-            switch (escolha){
+            switch (escolha) {
                 case "1":
                     cadastrarProjetos();
                 case "2":
                     mostrarProjetos();
             }
+        }
     }
 
     public void cadastrarProjetos(){
@@ -25,6 +25,14 @@ public class Main{
         double salarioGerente = Double.parseDouble(IO.readln("Digite o salario do gerente: "));
         String nomeProjeto = IO.readln("Digite o nome do projeto: ");
         Gerente gerente = new Gerente(salarioGerente, nomeGerente);
+
+        int qtdeSubordinados = Integer.parseInt(IO.readln("Digite a qtde. de subordinados: "));
+
+        for (int i = 0; i < qtdeSubordinados; i++) {
+            double salario = Double.parseDouble(IO.readln("Digite o salario do funcionario " + i+1 +" :"));
+            funcionarios[i] = new Subordinado(salario);
+        }
+
         Projeto projeto = new Projeto(nomeProjeto, gerente, funcionarios);
     }
 
