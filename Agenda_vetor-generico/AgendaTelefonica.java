6f1bcdd548c382f35aded9f04e2ba1749007a959
kaleu-b <kaleu.b@aluno.ifsc.edu.br>
2026-08-13 20:11:20 -0300

    aDAPTANDO A AGENDA

---
+++ Agenda_vetor-generico/AgendaTelefonica.java
@@ -0,0 +1,128 @@
+public class AgendaTelefonica {
+    private Vetor<Contato> contatos;
+    private int tamanho;
+
+    public AgendaTelefonica(int quantidade) {
+        contatos = new Vetor<>(quantidade);
+        this.tamanho = 0;
+    }
+
+    public void adicionar(Contato contato) {
+        for (int i = 0; i < tamanho; i++) {
+            if (contatos[i].getNome().equals(contato.getNome())) {
+                System.out.println("Nome repetido!");
+                return;
+            }
+            if (contatos[i].getTelefones().equals(contato.getTelefones())) {
+                System.out.println("Telefone repetido!");
+                return;
+            }
+        }
+
+        if (tamanho < contatos.length) {
+            contatos[tamanho] = contato;
+            tamanho++;
+        } else {
+            System.out.println("Não existe posição disponivel");
+        }
+        expandir();
+    }
+
+    public void remover(int indice) {
+        if (indice < 0 || indice >= tamanho) {
+            System.out.println("Indice Inválido");
+            return;
+        }
+        for (int i = indice; i < tamanho; i++) {
+            contatos[i] = contatos[i+1];
+        }
+        contatos[tamanho-1] = null;
+        tamanho--;
+        reduzir();
+    }
+
+    public void remover(Contato contato) {
+        for (int i = 0; i < tamanho; i++) {
+            if (contatos[i].getNome().equals(contato.getNome()) || contatos[i].getTelefones().equals(contato.getTelefones())) {
+                remover(i);
+            }
+            contatos[tamanho-1] = null;
+            tamanho--;
+            return;
+        }
+        reduzir();
+    }
+
+    public void buscar(Contato contato) {
+        for (int i = 0; i < tamanho; i++) {
+            if (contatos[i].getNome().equals(contato.getNome()) || contatos[i].getTelefones().equals(contato.getTelefones())) {
+                System.out.println("Nome: " + contatos[i].getNome() + " Telefone: " + contatos[i].getTelefones());
+                return;
+            }
+        }
+    }
+
+    public Contato[] buscarprefixo(String nome) {
+        Contato[] contatosIguais = new Contato[tamanho];
+        int tamanhoInicial = 0;
+        for (int i = 0; i < tamanho; i++) {
+            if (contatos[i].getNome().startsWith(nome)) {
+                contatosIguais[tamanhoInicial] = contatos[i]; 
+                tamanhoInicial++;
+            }
+        }
+        return contatosIguais;
+    }
+
+    public void atualizar(Contato contatoantigo, Contato contatonovo) {
+        for (int i = 0; i < tamanho; i++) {
+            if (contatos[i].getNome().equals(contatoantigo.getNome()) || contatos[i].getTelefones().equals(contatoantigo.getTelefones())) {
+                contatos[i] = contatonovo;
+                return;
+            }
+        }
+    }
+
+    public void listar() {
+        System.out.print("[ ");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("Nome: " + contatos[i].getNome() + " Telefone: " + contatos[i].getTelefones());
+            if (i < contatos.length - 1) {
+                System.out.println("");
+            }
+        }
+        System.out.print(" ]");
+        System.out.println("");
+    }
+
+    public void manipulacao(Contato[] contato) {
+        if ( (tamanho + contato.length) > contatos.length ){
+            IO.print("Adicionando mais contatos do que pode.");
+        }else{
+            for (int i = 0; i < contato.length; i++){
+                adicionar(contato[i]);
+            }
+        }   
+    }
+
+
+      private void expandir(){
+        String[] novo = new String[contatos.length*2];
+        for (int i = 0; i < contatos.length; i++) {
+            novo[i] = contatos[i];
+        }
+        this.contatos = novo;
+    }
+
+    private void reduzir(){
+        if (tamanhoPreenchido <= elementos.length/4) {
+            // quanto diminuir??????????????? **METADE**!!!!!!!!!!!!!!!
+            String[] novo = new String[elementos.length / 2];
+            for (int i = 0; i < tamanho; i++) {
+                novo[i] = contatos[i];
+            }
+            this.contatos = novo;
+        }
+    }
+
+}
---
+++ Agenda_vetor-generico/Vetor.java
@@ -0,0 +1,67 @@
+public class Vetor<T> {
+
+    private T[] elementos;
+    private int tamanhoPreenchido;
+
+    @SuppressWarnings("unchecked")
+    public Vetor(int quantidade){
+        elementos = (T[]) new Object[quantidade];
+        tamanhoPreenchido = 0;
+    }
+
+    public void inserir(T elemento){
+        if(tamanhoPreenchido == elementos.length) {
+            expandir();
+        }
+        elementos[tamanhoPreenchido] = elemento;
+        tamanhoPreenchido++;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void expandir(){
+        T[] novo = (T[]) new Object[elementos.length*2];
+        for (int i = 0; i < elementos.length; i++) {
+            novo[i] = elementos[i];
+        }
+        this.elementos = novo;
+    }
+
+    public void imprimir(){
+        System.out.print("[");
+        for (T elemento : elementos) {
+            System.out.print(elemento + ",");
+        }
+        System.out.println("]");
+    }
+
+    @SuppressWarnings("unchecked")
+    private void reduzir(){
+        if (tamanhoPreenchido <= elementos.length/4) {
+            // quanto diminuir??????????????? **METADE**!!!!!!!!!!!!!!!
+            T[] novo = (T[]) new Object[elementos.length / 2];
+            for (int i = 0; i < tamanhoPreenchido; i++) {
+                novo[i] = elementos[i];
+            }
+            elementos = novo;
+        }
+    }
+
+    public void remover(int indice){
+        if (indice < 0 || indice > elementos.length){
+            System.out.println("Posição inválida");
+            return;
+        }
+
+        for(int i = indice; i < tamanhoPreenchido; i++){
+            elementos[i] = elementos[i+1];
+        }
+        elementos[tamanhoPreenchido-1] = null;
+        tamanhoPreenchido--;
+        reduzir();
+    }
+
+    public int obterTamanho(){
+        return elementos.length;
+    }
+
+}
