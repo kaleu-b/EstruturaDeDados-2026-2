6b67cf12ca21c32512a55115e4f7839d635afa0a
kaleu-b <kaleu.b@aluno.ifsc.edu.br>
2026-08-20 20:03:35 -0300

    adicionando arquivos reusados da aula3

---
+++ atividade02/untitled/src/main/java/Vetor.java
@@ -0,0 +1,140 @@
+public class Vetor<T> {
+
+    private T[] elementos;
+    private int tamanhoPreenchido;
+
+    @SuppressWarnings("unchecked")
+    public Vetor(int quantidade){
+        elementos = (T[]) new Object[quantidade];
+        tamanhoPreenchido = 0;
+    }
+
+    public void inserir(T elemento){
+        if(tamanhoPreenchido == elementos.length) {
+            expandir();
+        }
+        elementos[tamanhoPreenchido] = elemento;
+        tamanhoPreenchido++;
+    }
+
+    public T get(int i){
+        return elementos[i];
+    }
+
+    @SuppressWarnings("unchecked")
+    private void expandir(){
+        T[] novo = (T[]) new Object[elementos.length*2];
+        for (int i = 0; i < elementos.length; i++) {
+            novo[i] = elementos[i];
+        }
+        this.elementos = novo;
+    }
+
+    public void imprimir(){
+        System.out.print("[");
+        for (T elemento : elementos) {
+            System.out.print(elemento + ",");
+        }
+        System.out.println("]");
+    }
+
+    @SuppressWarnings("unchecked")
+    private void reduzir(){
+        if (tamanhoPreenchido <= elementos.length/4) {
+            // quanto diminuir??????????????? **METADE**!!!!!!!!!!!!!!!
+            T[] novo = (T[]) new Object[elementos.length / 2];
+            for (int i = 0; i < tamanhoPreenchido; i++) {
+                novo[i] = elementos[i];
+            }
+            elementos = novo;
+        }
+    }
+
+    public void remover(int indice){
+        if (indice < 0 || indice > elementos.length){
+            System.out.println("Posição inválida");
+            return;
+        }
+
+        for(int i = indice; i < tamanhoPreenchido; i++){
+            elementos[i] = elementos[i+1];
+        }
+        elementos[tamanhoPreenchido-1] = null;
+        tamanhoPreenchido--;
+        reduzir();
+    }
+
+    // retorna um booleano caso não seja necessária a posição do
+    // item existente
+    public boolean contem(T elemento) {
+        return localizar(elemento) != -1;
+    }
+
+    public void inserirOrdenadov2(T valor) {
+
+        if (localizar(valor) != -1) {
+            System.out.println("Valor " + valor + " já existe na lista.");
+            return;
+        }
+        if (tamanhoPreenchido == 0) {
+            inserir(valor, tamanhoPreenchido);
+            return;
+        }
+        for (int i = 0; i < tamanhoPreenchido; i++) {
+            if ((Integer)valor < (Integer) elementos[i]) {
+                inserir(valor,i);
+                break;
+            }
+        }
+    }
+
+    public void inserir(T elemento, int index){
+        if(index> elementos.length || index<0)return;
+
+        if(tamanhoPreenchido == elementos.length){
+            expandir();
+        }
+        // deslocamento
+        for (int i = tamanhoPreenchido; i > index; i--) {
+            elementos[i] = elementos[i-1];
+        }
+
+        elementos[index] = elemento;
+        tamanhoPreenchido++;
+    }
+
+    public int localizar(T elemento) {
+        for (int i = 0; i < tamanhoPreenchido; i++) {
+            if (elementos[i] != null && elementos[i] == elemento) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void inserirOrdenado(T valor) {
+        if (localizar(valor) != -1) {
+            System.out.println("Valor " + valor + " já existe na lista.");
+            return;
+        }
+        if (tamanhoPreenchido == elementos.length) {
+            expandir();
+        }
+        int i;
+        for (i = tamanhoPreenchido - 1; i >= 0; i--) {
+            Integer atual = (Integer) elementos[i];
+            if (atual > (Integer) valor) {
+                elementos[i + 1] = elementos[i]; // desloca para a direita
+            } else {
+                break;
+            }
+        }
+        elementos[i + 1] = valor;
+        tamanhoPreenchido++;
+    }
+
+    public int obterTamanho(){
+        return tamanhoPreenchido;
+    }
+
+}
---
+++ atividade02/untitled/src/main/java/VetorInteiros.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+
+public class VetorInteiros {
+
+    Random r = new Random(67);
+    public Vetor<Integer> vetor;
+
+    public VetorInteiros(int qtde){
+        vetor = new Vetor<>(qtde);
+    }
+
+    private int gerarNumero(int range){
+        return r.nextInt(range);
+    }
+
+    public void popular(int quantidade, int range){
+        while (vetor.obterTamanho() <= quantidade){
+            int sorteado = gerarNumero(range);
+            vetor.inserirOrdenadov2(sorteado);
+            //vetor.inserir(sorteado);
+        }
+    }
+
+    public void listar(){
+        vetor.imprimir();
+    }
+
+// busca linear com numeros inteiros
+    public int buscaLinear(Vetor<Integer> vetor, int alvo){
+        for (int i = 0; i < vetor.obterTamanho(); i++) {
+            if(vetor.get(i).equals(alvo)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+// busca linear para arranjos ordenados
+    public int buscaLinearOrdenada(Vetor<Integer> vetor, int alvo){
+        for (int i = 0; i < vetor.obterTamanho(); i++) {
+            if(vetor.get(i).equals(alvo)){
+                return i;
+            } else if (vetor.get(i) > alvo) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+// busca binaria
+    public int buscaBinaria(Vetor<Integer> vator, int alvo){
+        int inicio = 0;
+        int fim = vetor.obterTamanho();
+
+       while (inicio <= fim){
+
+           int meio = inicio+fim/2;
+
+           if(vetor.get(meio) == alvo){
+            return meio;
+           } else if (vetor.get(meio) > alvo) {
+               fim = meio-1;
+           }else {
+               inicio = meio+1;
+           }
+       }
+        return -1;
+    }
+}
