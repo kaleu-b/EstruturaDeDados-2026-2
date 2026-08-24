a0df8e3b076db94433492ca85ac412c8f4132950
Kaléu Borges Augusto <kaleu.b@aluno.ifsc.edu.br>
2026-08-23 23:44:48 -0300

    refatorando classe VetorInteiro e Main
    agora a classe VetorInteiro herda diretamente de Vetor, e cria um Vetor do tipo Integer.
    serve o propósito unico de ser uma classe pra criar Vetor do tipo Integer de forma mais concisa.
    metodos e atributos que eram de VetorInteiro (buscaLinear, tempo inicial e tempo final, etc.) foram movidos pra classe Main

--- atividade02/untitled/src/main/java/Main.java
+++ atividade02/untitled/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     // vetor com mil posicoes
     VetorInteiros vetorMil = new VetorInteiros(1000);
@@ -6,17 +8,121 @@ public class Main {
     // vetor com cem mil posicoes
     VetorInteiros vetorCemMil = new VetorInteiros(100000);
 
-    void main(){
-        // populando com numeros de forma ordenada
-        // como regra geral, o range vai ser
-        // igual ao valor da quantidade * 2
-        vetorMil.popular(1000, 2000);
-        vetorMil.buscaLinear(vetorMil, 0);
+    private long inicio;
+    private long fim;
+    private final Random random = new Random(67);
 
-        //vetorDezMil.popular(10000, 2000);
-        //vetorCemMil.popular(100000, 200000);
-        //vetorCemMil.buscaLinear(vetorCemMil, (100000-1));
-        //vetorMil.listar();
+    void main() throws InterruptedException {
+        IO.println("populando os vetores com numeros aleatorios: ");
+        Thread.sleep(2000);
+        IO.println("vetor de mil elementos: ");
+        Thread.sleep(1000);
+        popular(vetorMil, 1000, 2000);
+        IO.println("vetor de 10 mil elementos: ");
+        Thread.sleep(1000);
+        popular(vetorDezMil, 10000, 2000);
+        IO.println("vetor de 100 mil elementos: ");
+        Thread.sleep(1000);
+        popular(vetorCemMil, 100000, 200000);
+
+        buscaLinear(vetorMil, 0);
+
+        buscaBinaria(vetorCemMil, 100000 - 1);
     }
 
+    private void popular(Vetor<Integer> vetor, int quantidade, int range) {
+        while (vetor.obterTamanho() < quantidade) {
+            vetor.inserirOrdenado(random.nextInt(range));
+        }
+    }
+
+    // iremos calcular o tempo em ms de forma um pouco diferente
+    // ao inves do que temos no sigaa.
+    // ao inves de:
+    // long duracao = = (fim - inicio) / 1000000;
+    // usaremos um valor double para retorno.
+    // motivo: algumas operações levam menos de 1ms para serem concluidas
+    // e tipos double não tem casas decimais.
+    // ao usar double pra mostrar o tempo das operações
+    // temos mais precisão em casos onde a busca leva
+    // menos de 1ms para ser concluida,
+    // e mantemos precisão em casos onde a busca
+    private double calcTempo() {
+        return (double) (fim - inicio) / 1_000_000;
+    }
+    // lista os dados
+    private void listarEstatisticas(int comparacoes, int indice) {
+        IO.println(
+                String.format(
+                        """
+                        n° de comparações: %d
+                        tempo total em ms: %.2f
+                        indice do elemento: %d""",
+                        comparacoes, calcTempo(), indice
+                )
+        );
+    }
+    // busca linear para vetores não ordenados
+    public int buscaLinear(Vetor<Integer> vetor, int alvo) {
+        int comparacoes = 0;
+        inicio = System.nanoTime();
+        for (int i = 0; i < vetor.obterTamanho(); i++) {
+            comparacoes++;
+            if (vetor.get(i) == alvo) {
+                fim = System.nanoTime();
+                listarEstatisticas(comparacoes, i);
+                return i;
+            }
+        }
+        fim = System.nanoTime();
+        listarEstatisticas(comparacoes, -1);
+        return -1;
+    }
+    // busca linear para vetores ordenados
+    public int buscaLinearOrdenada(Vetor<Integer> vetor, int alvo) {
+        int comparacoes = 0;
+        inicio = System.nanoTime();
+        for (int i = 0; i < vetor.obterTamanho(); i++) {
+            comparacoes++;
+            if (vetor.get(i) == alvo) {
+                fim = System.nanoTime();
+                listarEstatisticas(comparacoes, i);
+                return i;
+            }
+            if (vetor.get(i) > alvo) {
+                fim = System.nanoTime();
+                listarEstatisticas(comparacoes, -1);
+                return -1;
+            }
+        }
+        fim = System.nanoTime();
+        listarEstatisticas(comparacoes, -1);
+        return -1;
+    }
+    // busca binaria
+    public int buscaBinaria(Vetor<Integer> vetor, int alvo) {
+        int comparacoes = 0;
+        int inicioBusca = 0;
+        int fimBusca = vetor.obterTamanho() - 1;
+        inicio = System.nanoTime();
+
+        while (inicioBusca <= fimBusca) {
+            comparacoes++;
+            int meio = (inicioBusca + fimBusca) / 2;
+
+            if (vetor.get(meio) == alvo) {
+                fim = System.nanoTime();
+                listarEstatisticas(comparacoes, meio);
+                return meio;
+            }
+            if (vetor.get(meio) > alvo) {
+                fimBusca = meio - 1;
+            } else {
+                inicioBusca = meio + 1;
+            }
+        }
+        fim = System.nanoTime();
+        listarEstatisticas(comparacoes, -1);
+        return -1;
+    }
 }
